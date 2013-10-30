c88e616c5ab82e5f49e9766023767f6c97e02a21
Craig <craig.frey@hotmail.com>
2013-10-29 20:46:38 -0500

    added resources for pages and posts

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,7 @@
 Blog::Application.routes.draw do
   devise_for :users
+  resources :pages
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +42,7 @@ Blog::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
